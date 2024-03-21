06a41203e49b0c73ee2de98f6648e3f250b60f45
akshat-gosain <akshat.gosain@gmail.com>
2024-03-21 19:20:28 +0530

    code

--- DSA_C++_practice/striver/9.1Stack.cpp
+++ DSA_C++_practice/striver/9.1Stack.cpp
@@ -28,6 +28,49 @@ class Stack {
   }
 };
 
+int largestRectangleArea(vector<int> &heights){
+  int n = heights.size();
+  stack<int> st;
+  vector<int> left(n, -1);
+  vector<int> right(n, n);
+  
+  //left
+  for(int i=0; i<n; i++){
+    while(!st.empty() && heights[st.top()] >= heights[i]){
+      st.pop();
+    }
+    if(!st.empty()){
+      left[i] = st.top();
+    }
+    st.push(i);
+  }
+
+  while(!st.empty()){
+    st.pop();
+  }
+
+  //right
+  for(int i=n-1; i>=0; i--){
+    while(!st.empty() && heights[st.top()] >= heights[i]){
+      st.pop();
+    }
+    if(!st.empty()){
+      right[i] = st.top();
+    }
+    st.push(i);
+  }
+
+  //area
+  int maxArea = 0;
+  for(int i=0; i<n; i++){
+    maxArea = max(maxArea, heights[i]*(right[i]-left[i]-1));
+  }
+  return maxArea;
+
+}
+
+
+
 int main() {
 
   Stack s;
