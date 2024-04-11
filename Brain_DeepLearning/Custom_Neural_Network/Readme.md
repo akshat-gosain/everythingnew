b71cadd2dc44f79c5a659cde3378d08f5bd1f44f
akshat-gosain <akshat.gosain@gmail.com>
2024-04-11 05:40:40 +0530

    code

---
+++ Brain_DeepLearning/Custom_Neural_Network/Readme.md
@@ -0,0 +1,14 @@
+# Neural Network From Scratch with Custom Categorical Cross Entropy
+
+## Abstract
+In this assignment, a one hidden layer neural network model that adapts to the most suitable activation function according to the dataset is developed. The mathematical formulation of the activation function used in this method is $$g(x)  = k_0 + k_1x$$, where $$x, g(x) \in \textbf{R}$$, and  $$k_0, k_1$$ are learned from the data via gradient descent. The performance of the one hidden layer neural network model is evaluated on Breast Cancer Wisconsin, Iris, Bank Note Authentication, and MNIST datasets.
+
+## Project Report
+The detailed report of this project is available in the PDF file [ImplementingCustomNeuralNetworkfromScratch.pdf](https://github.com/akshat-gosain/everythingnew/blob/master/Brain_DeepLearning/Custom_Neural_Network/ImplementingCustomNeuralNetworkFromScratch.pdf).
+
+## Datasets
+The model was implemented, evaluated and plotted on the following datasets:
+- Breast Cancer Wisconsin
+- Iris
+- Bank Note Authentication
+- MNIST
--- DSA_C++_practice/striver/15.1Graphs.cpp
+++ DSA_C++_practice/striver/15.1Graphs.cpp
@@ -29,6 +29,7 @@ vector<int> bfsOfGraph(int V, vector<int> adj[]){
             }
         }
     }
+    return bfs; // Added return statement
 }
 
 vector<int> dfsOfGraph(int V,vector<int> adj[]){
@@ -84,6 +85,42 @@ vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges){
     }
     return adjedges;
 }
+
+class Solution {
+private:
+    void dfs(int node, vector<vector<int>>& adjLs, vector<int>& vis){
+        vis[node]=1;
+        for(auto it : adjLs[node]){
+            if(!vis[it]){
+                dfs(it,adjLs,vis);
+            }
+        }
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<vector<int>> adjLs(V);
+        for(int i = 0;i<V;i++){
+            for(int j = 0;j<V;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
+                }
+            }
+        }
+        vector<int> vis(V, 0);
+        int count = 0;
+        for(int i = 0;i<V;i++){
+            if(!vis[i]){
+                count++;
+                dfs(i,adjLs,vis);
+            }
+        }
+        return count;
+    }
+};
+
+
 
 
 int main(){
